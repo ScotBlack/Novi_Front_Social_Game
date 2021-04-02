1db4d211a3a9b89a981dab21a455d47430665275
Ian Hupkes <ScotBlack@users.noreply.github.com>
2021-04-02 20:50:22 +0200

    -adding state, now loads player from database!

--- src/index.js
+++ src/index.js
@@ -16,17 +16,28 @@ import axios from "axios";
 
 
 class Playerz extends React.Component {
+    _isMounted = false;
 
     constructor(props) {
         super(props);
 
-        this.state = {  color: null,
-            name: null};
-
-        axios.get('http://localhost:8080/test/player/3', { timeout: 500})
-            .then(res => this.setState({color: res.data.color, name: res.data.name}))
+        this.state = {
+            color: null,
+            name: null
+        };
     }
 
+    componentDidMount() {
+        this._isMounted = true;
+
+        axios.get('http://localhost:8080/test/player/3', {timeout: 500})
+                .then(res => this.setState({color: res.data.color, name: res.data.name}))
+    }
+
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
 
     render() {
         return <article className={"player " + this.state.color} ><h2 className={"player_name"}>{this.state.name}</h2></article>
